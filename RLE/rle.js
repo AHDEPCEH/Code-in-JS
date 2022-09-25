8779923f7508f89030de3e3309a8e3f7013142b0
AHDEPCEH <114348208+AHDEPCEH@users.noreply.github.com>
2022-09-26 00:25:43 +0500

    Update rle.js

--- RLE/rle.js
+++ RLE/rle.js
@@ -1,18 +1,19 @@
 const fs = require("fs")
-let name = process.argv[2];
-let arg_func = process.argv[3];
-let str = fs.readFileSync(name, "utf8");
+let arg_func = process.argv[2]; //Берём аргумент code/decode
+let name1 = process.argv[3];//Имя читаемого файла
+let name2 = process.argv[4];//Имя записываемого файла
+let str = fs.readFileSync(name1, "utf8"); //Чтение строки из файла
 
 
-function coding(str){
-	let loc_str = str[0];
+function coding(str){ //Функция кодировки строки
+	let loc_str = str[0];//Подстроки главной строки
 	let i = 1;
-	let new_str = "";
+	let new_str = "";// Новая строка
 	let len = str.length;
-	while (i <= len) {
+	while (i <= len) { //Перебираем символы строки и собираем новую
 		if (str[i] == str[i - 1] & i < len & loc_str.length < 255) loc_str += str[i]; 
 		else {
-			if (loc_str.length < 4) new_str += loc_str;
+			if (loc_str.length < 4 & loc_str[0] != "#") new_str += loc_str;
 			else new_str += "#" + String.fromCharCode(loc_str.length) + loc_str[0];
 			loc_str = str[i];
 		}
@@ -22,11 +23,11 @@ function coding(str){
 }
 
 
-function decoding(str){
+function decoding(str){ // Функция декодирования строки
     let new_str = "";
     let i = 0;
     let len = str.length;
-    while (i < len){
+    while (i < len){ //Проверяем каждый символ, если встретим # то расшифровываем его 
         if (str[i] == "#"){
             i ++;
             for (j = 0; j < str.charCodeAt(i); j ++) new_str += str[i + 1];
@@ -39,12 +40,12 @@ function decoding(str){
 }
 
 
-if (arg_func.toLowerCase() == "coding"){
-    let content = coding(str);
-    const data = fs.writeFileSync("rleout.txt", content);
-    console.log(str.length / content.length);	
+if (arg_func.toLowerCase() == "code"){ //Проверям тип операции на code
+    let content = coding(str); //Получившаяся строка 
+    const data = fs.writeFileSync(name2, content); //Записываем в файл
+    console.log(str.length / content.length); //Выводим коэффицент сжатия 
 }
-if (arg_func.toLowerCase() == "decoding"){
-    let content = decoding(str);
-    const data = fs.writeFileSync("rleout.txt", content);
+if (arg_func.toLowerCase() == "decode"){ //Проверям тип операции на decode
+    let content = decoding(str); //Получившаяся строка
+    const data = fs.writeFileSync(name2, content); //Записываем в файл
 }
